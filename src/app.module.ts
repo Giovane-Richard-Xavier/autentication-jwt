0a9845bc28f4e111533530d5b27373b0998c593e
Giovane <giovane@rlvtecnologia.com.br>
2024-08-03 12:48:04 -0300

    feat: implement department crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { DepartmentModule } from './department/department.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule, PrismaModule, DepartmentModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { $Enums } from "@prisma/client";
+
+export class CreateUserDto {
+    full_name: string;
+    birtdate: string;
+    cpf: string;
+    password_hash: string;
+    email: string;
+    phone: string;
+    is_active: boolean;
+    role: $Enums.user_role;
+    department_id: string;
+    office_id: string;
+}
