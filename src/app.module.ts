f073a6ac974648418df5412164df215a5074605a
Giovane <giovane@rlvtecnologia.com.br>
2024-08-03 12:49:10 -0300

    feat: implement office crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { DepartmentModule } from './department/department.module';
+import { OfficeModule } from './office/office.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, DepartmentModule],
+  imports: [UsersModule, PrismaModule, DepartmentModule, OfficeModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
